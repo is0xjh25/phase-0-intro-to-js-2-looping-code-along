73d87a8c9798852c8f69db1babba7b1838a7e3dd
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-04 13:05:01 +0800

    Update: Test passed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(arr, m) {
+	
+	const newArr = [];
+
+	arr.forEach(e => {
+		newArr.push(`Thank you, ${e}, for the wonderful ${m} gift!`);
+	});
+
+	return newArr;
+}
+
+function  countDown(i) {
+	while (i >= 0) {
+		console.log(i);
+		i -= 1;
+	}
+}
